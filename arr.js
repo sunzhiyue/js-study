d83a17fbe08bb5237f27608f701e541c4955fe7a
sunzhiyue <sunzhiue@outlook.com>
2017-09-14 15:46:46 +0800

    arr的各种用法

---
+++ arr.js
@@ -0,0 +1,38 @@
+/**
+ * 2017-9-14  sun zhiyue
+ */
+var arr = [1,2];
+
+console.log(arr.push(3,4));
+
+console.log(arr);
+
+console.log(arr.pop());
+
+console.log(arr);
+
+console.log(arr.unshift('a','b'));
+
+console.log(arr);
+
+console.log(arr.shift());
+
+console.log(arr);
+
+console.log(arr.sort());
+
+console.log(arr.reverse());
+
+console.log(arr.unshift('4','7','6','8'));
+
+console.log(arr);
+
+console.log(arr.push(-1,-3,-5));
+
+console.log(arr.sort());
+
+console.log(arr.reverse());
+
+console.log(arr.indexOf(3));
+
+console.log(arr[9]='s');
