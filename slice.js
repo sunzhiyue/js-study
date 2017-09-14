aff2f96e43a14d8924b445b83320b2cc8c099298
sunzhiyue <sunzhiue@outlook.com>
2017-09-14 15:08:50 +0800

    slice()的使用方法和截取数组的部分元素返回新数组和多行字符串的使用

---
+++ slice.js
@@ -0,0 +1,27 @@
+/**
+ * 2017-9-14   sun zhiyue
+ */
+var arr = ['a','b','c','d','e','f','g'];
+
+console.log(`arr.slice(0,4)=> [ ${arr.slice(0,4)} ]`);
+
+console.log(`arr.slice(4)=> [ ${arr.slice(4)} ]`); 
+
+console.log(`arr.slice(-1)=> [ ${arr.slice(-1)} ]`);
+
+console.log(arr.slice(7));
+
+console.log(arr.slice(-1,2));
+
+console.log(arr.slice(-2));
+
+console.log(`arr.slice(-1,-2)=> [ ${arr.slice(-1,-2)} ]`);
+
+console.log(arr.slice(4,2));
+
+console.log(`arr.slice(2,4)=> [ ${arr.slice(2,4)} ]`);
+
+console.log(`arr.slice(3,-1)=> [ ${arr.slice(3,-1)} ]`);
+
+console.log(`arr.slice(3,-3)=> [ ${arr.slice(3,-3)} ]`);
+
