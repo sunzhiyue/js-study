1f5f5f1f74115c29b29e59183da5b4612bbcb243
sunzhiyue <sunzhiue@outlook.com>
2017-09-15 16:49:32 +0800

    驱动控制硬件

---
+++ driver.js
@@ -0,0 +1,82 @@
+/**
+ * 2017-9-15   sunzhiyue
+ */
+
+var device = ['/dev/conlose','/dev/dis' ];
+
+function open(dev){
+    for(var n = 0; n<=driver.length; n++){
+        driver[n].type === dev;
+        driver[n].open();
+        return n;
+        
+    }
+    
+}
+
+function read(fd){
+
+    return driver[fd].read() ;
+}
+
+function write(fd,conlose){
+    return driver[fd].write(conlose) ;
+}
+
+function close(fd){
+    return driver[fd].close() ;
+}
+
+var driver =[{
+    type : '/dev/conlose',
+    open : function(){
+        console.log('open')
+
+    },
+
+    write : function(conlose){
+        console.log(`${conlose}`)
+    },
+
+    read : function(){
+        console.log('read')
+    },
+
+    close : function(){
+        console.log('close')
+
+    }
+
+}];
+
+var driver =[{
+    type : '/dev/display',
+    open : function(){
+        console.log('open')
+
+    },
+
+    write : function(conlose){
+         console.log(`${conlose}`)
+    },
+
+    read : function(){
+        console.log('read')
+    },
+
+    close : function(){
+        console.log('close')
+
+    }
+
+}];
+
+var fd = open('/dev/console');
+write(fd, 'hi');
+read(fd);
+close(fd);
+
+var fd = open('/dev/dis');
+write(fd, 'hello');
+//read(fd);
+close(fd);
