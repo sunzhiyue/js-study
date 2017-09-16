b9607d40534281a9428b1246eb3aa421804a5d73
sunzhiyue <sunzhiue@outlook.com>
2017-09-16 15:39:18 +0800

    assgin的使用方法

---
+++ assgin.js
@@ -0,0 +1,17 @@
+/**
+ * 2017-9-16 sunzhiyue
+ */
+var o = Object.create(null, {
+    foo: {
+        enumerable: false,
+        value:'hello'
+    },
+    bar:{
+        enumerable: true,
+        value: 'yi'
+    }
+});
+var obj = {};
+Object.assign(obj,o);
+console.log(o, obj);
+console.log(o.foo, o.bar);
