d28895e3e083069a96078ddb94c47bf691b7acb8
sunzhiyue <sunzhiue@outlook.com>
2017-09-16 19:33:59 +0800

    definePropery 的使用方法

--- property.js
+++ property.js
@@ -1,11 +1,52 @@
 /**
  * 2017-9-14   sunzhiyue
  */
-var o ={
+/*var o ={
     name: 'hello',
     age: 20,
     city: 'hangzhou',
 };
 for (var key in o){
     console.log(key, o[key]);
-};
+};*/
+
+var o = Object.create(null, {
+    foo: {
+        enumerable: true,
+        writable: true,
+        configurable: true,
+        value: 'hello'
+    }
+});
+
+var obj = Object.defineProperty(o, 'foo',{
+    enumerable: true,
+    writable: true,
+    configurable: true,
+    value:'hi'
+})
+
+console.log( obj);
+
+var arr = [1, 2];
+var obje = Object.defineProperty(arr,'length',{
+    enumerable: true,
+    writable: true,
+    configurable: true,
+    value: 1
+}
+)
+var obje = Object.defineProperty(arr,  '4',{
+    enumerable: true,
+    writable: true,
+    configurable: true,
+    value: 128
+}
+)
+console.log(arr)
+
+var a = {
+    n: 1,
+
+}
+console.log(a['0'])
