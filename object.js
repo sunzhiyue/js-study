c21d6047f58cd49a5c15c3907a88ed76e7d3dc5b
sunzhiyue <sunzhiue@outlook.com>
2017-09-16 11:49:24 +0800

    object 练习

--- object.js
+++ object.js
@@ -13,5 +13,46 @@ var obj = {
     
 };
 
-obj.da();
+obj.da(); 
 
+var o = Object.create(null, {
+    foo: {
+        writable: true,
+        configurable: true,
+        value: 'hello' 
+    },
+    bar: {
+        configurable: false,
+        get: function() {return 10;},
+        set: function(value){
+            console.log('Setting `o.bar` to', value);
+        }
+    }
+});
+o.name = 1;
+console.log(Object.keys(o));
+console.log(`${typeof o} is type`);
+console.log(Object.getPrototypeOf(o));
+console.log(Object.getOwnPropertyDescriptor(o,'foo'))
+console.log(JSON.stringify(Object.getOwnPropertyDescriptor(o,'bar')))
+console.log(JSON.stringify(Object.getOwnPropertyDescriptor(o,'foo')))
+
+var arr = ['a', 'b', 'c'];
+arr['name'] = 'sunzhiyue';
+arr.age = 22;
+console.log(Object.keys(arr));
+console.log(arr[0]);
+console.log(arr.name);
+console.log(`${typeof arr} is type`);
+console.log(Object.getPrototypeOf(arr));
+
+var fun = function (){
+   console.log('annn');
+};
+
+fun.age = 23;
+console.log(Object.keys(fun));
+console.log(fun.age);
+fun()
+console.log(`${typeof fun} is type`);
+console.log(Object.getPrototypeOf(fun));
