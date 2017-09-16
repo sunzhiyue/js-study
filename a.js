77c1482a345fe088e9b8f413fd680cccb9a80a42
sunzhiyue <sunzhiue@outlook.com>
2017-09-16 21:27:13 +0800

    get 的使用

---
+++ a.js
@@ -0,0 +1,25 @@
+/**
+ * 2017-9-16   sunzhiyue
+ */
+var b = function(){
+
+};
+b.arr = [1, 2, 3, 4, 5];
+b.obj = {
+    name : 'wan',
+    age : 18,
+    car : true
+}
+b.fun = function(){
+    console.log('fun');
+}
+
+Object.defineProperty(b,'len',{
+    enumerable: true,
+    configurable: false,
+    get: function() { return this.arr.length; }
+}
+
+)
+b.arr.length = 10;
+console.log(b.len);
