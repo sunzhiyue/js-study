73eec1b0302e33a819bbc3b0a3f5511baac12231
sunzhiyue <sunzhiue@outlook.com>
2017-09-17 13:41:40 +0800

    闭包

--- closure.js
+++ closure.js
@@ -1,6 +1,7 @@
 /**
  * 2017-9-17  sunzhiyue
  */
+//除法
 function div(x){
     return function (y){
         return x / y;
@@ -8,3 +9,13 @@ function div(x){
 }
 var x2 = div(2);
 console.log(x2(4));
+
+
+//乘法
+function mul(x){
+    return function (y){
+        return x * y;
+    }
+}
+var x3 = mul(3);
+console.log(x3(5));
