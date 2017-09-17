0eb927b4cd75080c8a9cbd8fa513f5c70308fb6a
sunzhiyue <sunzhiue@outlook.com>
2017-09-17 20:52:13 +0800

    闭包

--- closure.js
+++ closure.js
@@ -19,3 +19,32 @@ function mul(x){
 }
 var x3 = mul(3);
 console.log(x3(5));
+
+var i = 1;
+function b(){
+    
+    var a = {
+        age: i++
+    };
+    return a
+    
+};
+
+console.log(b(), b(), b(), b());
+
+
+var func = function (g){
+    var i = 1;
+    return function(){
+        var obj1 = {
+            age1: i
+        };
+
+        i = i + g;
+        return obj1;
+    };
+    
+};
+var f = func(1);
+console.log(f(), f(), f(), f(), f());
+
