0ae393f8a7a45c35f190332585bf860063362d61
sunzhiyue <sunzhiue@outlook.com>
2017-09-18 16:55:10 +0800

    99乘法表

--- mul.js
+++ mul.js
@@ -7,12 +7,20 @@ for(var k = 0; k < 9; k++){
 };
 console.log(arr1);
 for(var i = 1; i <= 9; i++){
-    var arr = []
-    
+    var arr = [''];
     for(var j = 1;j <= i; j++){
-        arr = arr + ` ${j * i}\t`;
+        arr.push(`${j * i}\t`);
     };
     arr = `${i}\t` + arr
      console.log(arr);
 };
-
+console.log('-----------------------------------------------------------------------------')
+console.log('\t1\t2\t3\t4\t5\t6\t7\t8\t9')
+for(var q = 1; q <=9; q++){
+    var str = '';
+    for (var w = 1;w <= q; w++){
+        str = str + `${q * w}\t`
+    };
+    str = `${q}\t` + str;
+    console.log(str);
+}
