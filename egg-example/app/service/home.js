84bce942d6d47d471c00182482ace23ca321b8ec
sunzhiyue <sunzhiue@outlook.com>
2017-09-24 20:46:54 +0800

    egg的服务-home

--- egg-example/app/service/home.js
+++ egg-example/app/service/home.js
@@ -5,6 +5,21 @@ module.exports = app => {
     * find() {
       return '1';
     }
+    * sina() {
+      return yield app.curl('https://www.sina.com.cn');
+    }
+    * sohu() {
+      return yield app.curl('https://sohu.com');
+    }
+    * test() {
+      return 'home';
+    }
+    * date() {
+      const d = new Date();
+      return `${d.getFullYear()} - ${d.getMonth() + 1} - ${d.getDate()} - ${d.getHours()} : ${d.getMinutes()} : ${d.getSeconds()}`;
+    }
   }
+
   return Home;
 };
+
