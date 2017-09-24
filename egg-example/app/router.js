d70837c443de7272da3abc150cefae9644b74add
sunzhiyue <sunzhiue@outlook.com>
2017-09-24 20:44:10 +0800

    egg的路由

--- egg-example/app/router.js
+++ egg-example/app/router.js
@@ -2,5 +2,13 @@
 
 module.exports = app => {
   app.get('/', 'home.index');
-  app.get('/home', 'home.a');
+  app.get('/sina', 'home.sina');
+  app.get('/sohu', 'home.sohu');
+  app.get('/test', 'home.test');
+  app.get('/apple', 'home.apple');
+  app.get('/lodash', 'home.lodash1');
+  app.get('/cx', 'home.cx');
+  app.get('/help', 'home.help');
+  app.post('/help', 'home.help');
+  app.post('/date', 'home.date');
 };
